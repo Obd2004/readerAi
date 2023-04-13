0b9ec88954a80dbeffb0207db44b3f4fb474a286
Obd2004 <obidjonabd1097@gmail.com>
2023-04-13 22:05:31 +0500

    reader ai

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useRef, useState } from 'react';
+import Header from './Main/Header';
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { ChakraProvider } from '@chakra-ui/react';
+import './Main/faq.css';
+import Faqss from './Main/Faq'
 function App() {
+
+  const alanBtnInstance = useRef(null);
+  const [index, setIndex] = useState(null);
+
+  useEffect(() => {
+      if (!alanBtnInstance.current) {
+          alanBtnInstance.current = alanBtn({
+              key: 'b5d9e577b0305f361fdce6ca2010011a2e956eca572e1d8b807a3e2338fdd0dc/stage',
+              onCommand: (commandData) => {
+              if (commandData.command === 'gotoFaq') {
+                  setIndex(commandData.faqId - 1)
+                }
+              }
+          });
+      }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div class="alan-btn"></div>
+      <ChakraProvider>
+        <Faqss index={index} setIndex={setIndex}/>
+      </ChakraProvider>
     </div>
   );
 }
---
+++ src/Main/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function Header() {
+  return (
+    <div className='header'>
+        <h4>FAQ about Uzbekiston</h4>
+    </div>
+  )
+}
+
+export default Header
